1e1f7d8f05640d65be175e752db4fcc0d44b294b
Chris Angelico <rosuav@gmail.com>
2018-11-06 00:21:27 +1100

    Jot down some features I might be using

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+/* Stuff I might be able to make use of:
+* https://developer.chrome.com/extensions/user_interface#context_menu
+  - Right-click on anything to react to the selected text, even inside an input/textarea
+* https://developer.chrome.com/extensions/content_scripts
+  - Run a script on every page, once it's loaded, and look for references
+*/
 chrome.runtime.onInstalled.addListener(function() {
 	console.log("Hello, world!");
 });
